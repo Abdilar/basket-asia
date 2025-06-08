9b10fd944e271abf7a19353709092abbc7356de3
Saeed Abdilar <40013688+Abdilar@users.noreply.github.com>
2025-06-09 00:53:48 +0330

    style(prettier): run prettier for better styling codes

--- src/layouts/Main.layout.tsx
+++ src/layouts/Main.layout.tsx
@@ -1,4 +1,4 @@
-import { Navbar } from "components"
+import { Navbar } from 'components'
 import { Outlet } from 'react-router-dom'
 
 export const MainLayout = () => {
--- src/pages/home/Home.page.tsx
+++ src/pages/home/Home.page.tsx
@@ -1,3 +1,3 @@
 export const HomePage = () => {
-  return <h2 data-testid="home">📄 Customer list</h2>
+  return <h2 data-testid='home'>📄 Customer list</h2>
 }
