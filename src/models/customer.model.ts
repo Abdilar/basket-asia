98825e81760b0fb500e2be04f2864c19d61d21b8
Saeed Abdilar <40013688+Abdilar@users.noreply.github.com>
2025-06-09 00:15:35 +0330

    style(prettier): run prettier for better styling codes

--- src/models/customer.model.ts
+++ src/models/customer.model.ts
@@ -1,8 +1,8 @@
 export interface Customer {
-  firstName: string;
-  lastName: string;
-  dateOfBirth: Date;
-  phoneNumber: string;
-  email: string;
-  bankAccountNumber: string;
+  firstName: string
+  lastName: string
+  dateOfBirth: Date
+  phoneNumber: string
+  email: string
+  bankAccountNumber: string
 }
