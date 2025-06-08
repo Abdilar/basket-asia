f2ccf50092d8d660284f4ac37e754876a60a2b82
Saeed Abdilar <40013688+Abdilar@users.noreply.github.com>
2025-06-09 00:28:59 +0330

    feat: add main layout

---
+++ src/layouts/Main.layout.tsx
@@ -0,0 +1,15 @@
+import { Navbar } from "components"
+import { Outlet } from 'react-router-dom'
+
+export const MainLayout = () => {
+  return (
+    <>
+      <header>
+        <Navbar />
+      </header>
+      <main>
+        <Outlet />
+      </main>
+    </>
+  )
+}
